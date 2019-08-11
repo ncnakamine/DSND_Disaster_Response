98f7dbeb7c807f57fafdfc5f0fa4aaa7a107a4cb
Natalie Nakamine <ncnakamine@gmail.com>
2019-08-11 14:00:15 -0700

    Update create_visuals.py
    remove unnecessary load data returns

--- figures/create_visuals.py
+++ figures/create_visuals.py
@@ -25,10 +25,10 @@ def main():
         model = load_model(model_filepath)
 
         print('loading data...')
-        Y, Y_genre, X_test, Y_test, pred_actual_df = load_data(database_filepath, model)
+        Y, genre_df, pred_actual_df = load_data(database_filepath, model)
 
         print('creating visuals...')
-        create_visuals(Y, Y_genre, pred_actual_df)
+        create_visuals(Y, genre_df, pred_actual_df)
 
     else:
         print('Please provide the filepaths of the classifier pickle file '\
@@ -78,16 +78,16 @@ def load_model(model_filepath):
 
 def load_data(database_filepath, model):
     """
-    Load data necessary for visuals
+    Load and format data necessary for visuals
     :param database_filepath:
     :param model:
-    :return: Y, Y_genre, X_test, Y_test, pred_actual_df
+    :return: Y, genre_df, pred_actual_df
     """
 
     df = pd.read_sql_table('messages', 'sqlite:///{}'.format(database_filepath))
     X = df['message']
 
-    Y_genre = df[['genre']]
+    genre_df = df[['genre']]
 
     non_y_cols = ['id', 'message', 'original', 'genre']
     y_cols = [col for col in df.columns if col not in non_y_cols]
@@ -95,7 +95,7 @@ def load_data(database_filepath, model):
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=2, test_size=0.25)
 
-    # create df for predicted vs actual counts
+    # create df for predicted vs actual counts visual
     Y_pred = model.predict(X_test)
     Y_pred_sums = [Y_pred[0:, i].sum() for i in range(len(Y.columns))]
     Y_test_sums = [Y_test[c].sum() for c in Y.columns]
@@ -104,10 +104,10 @@ def load_data(database_filepath, model):
     pred_actual_df.index = pred_actual_df['Categories']
     pred_actual_df.drop('Categories', axis=1, inplace=True)
 
-    return Y, Y_genre, X_test, Y_test, pred_actual_df
+    return Y, genre_df, pred_actual_df
 
 
-def create_visuals(Y, Y_genre, pred_actual_df):
+def create_visuals(Y, genre_df, pred_actual_df):
 
     """
     create and save visuals
@@ -117,7 +117,7 @@ def create_visuals(Y, Y_genre, pred_actual_df):
     :return:
     """
 
-    Y_counts = [Y[i].sum() for i in Y.columns]
+    Y_counts = [Y[c].sum() for c in Y.columns]
     plt.barh(y=Y.columns, width=Y_counts, color = 'b')
     plt.xlabel('Counts')
     plt.title('Category Counts')
@@ -126,7 +126,7 @@ def create_visuals(Y, Y_genre, pred_actual_df):
     print('figure saved to figures/Category_Counts.png')
     plt.clf()
 
-    plt.bar(x=Y_genre['genre'].value_counts().index, height=Y_genre['genre'].value_counts(), color='b')
+    plt.bar(x=genre_df['genre'].value_counts().index, height=genre_df['genre'].value_counts(), color='b')
     plt.xlabel('Genre')
     plt.ylabel('Counts')
     plt.title('figures/Genre Counts')
