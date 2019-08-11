295357bfb76218c71350b118c06810270593b7ca
Natalie Nakamine <ncnakamine@gmail.com>
2019-08-11 14:08:03 -0700

    Update run.py
    import re

--- app/run.py
+++ app/run.py
@@ -10,7 +10,7 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
-
+import re
 
 app = Flask(__name__)
 
